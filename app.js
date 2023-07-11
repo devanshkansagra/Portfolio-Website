771c6cc804f49e81089b1056e44130b8f7bd36ce
Devansh <dkansagra04@gmail.com>
2023-07-11 23:03:06 +0530

    Added images and files to their specific folder and rendered webpage using nodejs

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-      res.send('Hello World! This is nodejs webserver')
-})
+const staticPath = path.join(__dirname,'/public')
+
+app.use(express.static(staticPath));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
