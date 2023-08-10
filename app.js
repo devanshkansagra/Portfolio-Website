bb0969ff71821609455ec6bb11f37f3b4a909f74
Devansh <dkansagra04@gmail.com>
2023-08-11 00:11:43 +0530

    Added feature of downloading cv through backend

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const path = require('path');
 const fs = require('fs');
 const app = express()
 const port = 80
+const download = fs.readFileSync('./Devansh_kansagra.pdf')
 
 const staticPath = path.join(__dirname,'/public')
 
 app.use(express.static(staticPath));
 app.use(express.urlencoded());
 
+// Downloading the resume
+app.get('/download', (req, res) => {
+    res.download('./Devansh_kansagra.pdf');
+})
+
+// Sending the details of contact form to the server
 app.post('/', (req, res) => {
     let name = req.body.fname;
     let email = req.body.email;
@@ -20,6 +27,7 @@ app.post('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', 'index.html'));
 })
 
+// Listening to the port
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
